84f5d41966585006e4e29b780b23dd9b85919f83
KevinGandhi9898 <72725226+KevinGandhi9898@users.noreply.github.com>
2022-04-12 05:18:59 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,107 +1,67 @@
-import React, {useState, useRef} from 'react';
-import {Container, Card, CardContent, makeStyles, Grid, TextField, Button} from '@material-ui/core';
+import React, { useState, useRef } from 'react';
+import { Container, Card, CardContent, makeStyles, Grid } from '@material-ui/core';
 import QRCode from 'qrcode';
 import QrReader from 'react-qr-reader';
 
 
-function App() { 
+function App() {
   const [text, setText] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  const [scanResultFile, setScanResultFile] = useState('');
-  const [scanResultWebCam, setScanResultWebCam] =  useState('');
+  const [scanResultFile, setScanResultFile] = useState(null);
+  const [scanResultWebCam, setScanResultWebCam] = useState('');
+  const [auhentic, setAuthentic] = useState(false);
   const classes = useStyles();
   const qrRef = useRef(null);
 
-
-  const generateQrCode = async () => {
-    try {
-          const response = await QRCode.toDataURL(text);
-          setImageUrl(response);
-    }catch (error) {
-      console.log(error);
-    }
-  }
-  const handleErrorFile = (error) => {
-    console.log(error);
-  }
-  const handleScanFile = (result) => {
-      if (result) {
-          setScanResultFile(result);
-      }
-  }
-  const onScanFile = () => {
-    qrRef.current.openImageDialog();
-  }
   const handleErrorWebCam = (error) => {
     console.log(error);
   }
   const handleScanWebCam = (result) => {
-    if (result){
-        setScanResultWebCam(result);
+    if (result) {
+      setScanResultWebCam(result);
     }
-   }
+  }
+
   return (
     <Container className={classes.conatiner}>
-          <Card>
-              <h2 className={classes.title}>Generate Download & Scan QR Code with React js</h2>
-              <CardContent>
-                  <Grid container spacing={2}>
-                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                          <TextField label="Enter Text Here" onChange={(e) => setText(e.target.value)}/>
-                          <Button className={classes.btn} variant="contained" 
-                            color="primary" onClick={() => generateQrCode()}>Generate</Button>
-                            <br/>
-                            <br/>
-                            <br/>
-                            {imageUrl ? (
-                              <a href={imageUrl} download>
-                                  <img src={imageUrl} alt="img"/>
-                              </a>) : null}
-                      </Grid>
-                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                        <Button className={classes.btn} variant="contained" color="secondary" onClick={onScanFile}>Scan Qr Code</Button>
-                        <QrReader
-                          ref={qrRef}
-                          delay={300}
-                          style={{width: '100%'}}
-                          onError={handleErrorFile}
-                          onScan={handleScanFile}
-                          legacyMode
-                        />
-                        <h3>Scanned Code: {scanResultFile}</h3>
-                      </Grid>
-                      <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-                         <h3>Qr Code Scan by Web Cam</h3>
-                         <QrReader
-                         delay={300}
-                         style={{width: '100%'}}
-                         onError={handleErrorWebCam}
-                         onScan={handleScanWebCam}
-                         />
-                         <h3>Scanned By WebCam Code: {scanResultWebCam}</h3>
-                      </Grid>
-                  </Grid>
-              </CardContent>
-          </Card>
+      <Card>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <h3 style={{fontSize: '2rem'}}>Fake Product Authenticator</h3>
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+                <QrReader
+                  delay={300}
+                  style={{ width: '20rem' }}
+                  onError={handleErrorWebCam}
+                  onScan={handleScanWebCam}
+                />
+              </Grid>
+            </Grid>
+            <h3 style={{textAlign: 'center'}}>Scanned By WebCam Code:</h3>
+            {{scanResultFile} && <h3 style={{textAlign: 'center'}}>{scanResultWebCam == 'http://en.m.wikipedia.org' ? 'The Product is authentic ✅' : 'The product is fake ❎'}</h3>}
+          </CardContent>
+        </div>
+      </Card>
     </Container>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
-    conatiner: {
-      marginTop: 10
-    },
-    title: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems:  'center',
-      background: '#3f51b5',
-      color: '#fff',
-      padding: 20
-    },
-    btn : {
-      marginTop: 10,
-      marginBottom: 20
-    }
+  conatiner: {
+    marginTop: 10
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: '#ffff',
+    color: 'black',
+    padding: 20
+  },
+  btn: {
+    marginTop: 10,
+    marginBottom: 20
+  }
 }));
 export default App;
